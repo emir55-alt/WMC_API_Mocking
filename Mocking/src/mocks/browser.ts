136ca69fdd573036eb28c8decc70815a6ed899b3
nemo828 <nes230256@spengergasse.at>
2026-03-13 10:25:31 +0100

    Kommentare bei Server.ts und Browser.ts

--- Mocking/src/mocks/browser.ts
+++ Mocking/src/mocks/browser.ts
@@ -2,4 +2,6 @@ import { setupWorker } from "msw/browser";
 import { handlers } from "./handlers";
 
 // MSW Service Worker für den Browser einrichten
+//Intercepted die fetches die vom frontend gemacht werden
+//und returned die "mocked" antworten mit den fake daten
 export const worker = setupWorker(...handlers);
--- Mocking/src/mocks/server.ts
+++ Mocking/src/mocks/server.ts
@@ -1,5 +1,8 @@
 import { setupServer } from "msw/node";
 import { handlers } from "./handlers";
 
-// MSW Server für Node.js (Tests)
+// Nutzt Msw in einem Node.js enviroment
+//Intercepted die Http request die bei den tests gemacht werden
+// Responeded mit den vordefinierten handlers
+//Anstatt das Backend wirklich zu callen
 export const server = setupServer(...handlers);
